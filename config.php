28c89e6af66977371b786a5aa3811b17ea80ab84
Fakoro <fakoro88@gmail.com>
2025-10-02 11:30:27 +0000

    Update DB_HOST in config.php to use port 3307 for database connection.

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Configuration de la base de données
-define('DB_HOST', 'localhost:3306');
+define('DB_HOST', 'localhost:3307');
 define('DB_NAME', 'ecole');
 define('DB_USER', 'root');
 define('DB_PASS', '');
